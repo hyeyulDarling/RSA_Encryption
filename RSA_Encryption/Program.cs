02e3fc8f97558d4625fc0ec7b33feb9771e29eb4
unknown <anielamilek@gmail.com>
2026-07-12 19:17:57 +0100

    Added a function that checks whether variable number (which will be p and q) is prime

--- RSA_Encryption/Program.cs
+++ RSA_Encryption/Program.cs
@@ -4,3 +4,18 @@ int q = int.Parse(Console.ReadLine());
 
 /* Add a function to check whether p and q are primes */ 
 
+public static bool IsPrime ( int number)
+{
+    if (number < 2)
+    {
+        return false;
+    }
+    for (int i = 2; i <= Math.Sqrt(number); i ++)
+    {
+        if (number % i == 0)
+        {
+            return false;
+        }
+    }
+    return true
+}
